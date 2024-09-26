15af5f66d01acbd6013487f13e1c23229122ff27
Kevin <kevinrc96@hotmail.com>
2024-09-26 14:47:32 +0200

    css responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,14 +6,14 @@ export function FooterComp () {
     return (
         <footer className='bot-page'>
         <div className='big-container'>
-        <h3 className="footer-title">Contact</h3>
+        <h3 className="footer-title">Contacto</h3>
             <div className="linkedin-container">
                 <img src={LogoLinkedin} alt="Logo Linkedin" />
-                <a href='https://www.linkedin.com/in/kevin-rodriguez-carames/' target='_blank'>https://www.linkedin.com/in/kevin-rodriguez-carames/</a>
+                <a className='contact-info' href='https://www.linkedin.com/in/kevin-rodriguez-carames/' target='_blank'>https://www.linkedin.com/in/kevin-rodriguez-carames/</a>
             </div>
             <div className="outlook-container">
                 <img src={LogoOutlook} alt="Logo Outlook" />
-                <p>kevinjob96@outlook.es</p>
+                <p className='contact-info' >kevinjob96@outlook.es</p>
             </div>
         </div>
         </footer>
